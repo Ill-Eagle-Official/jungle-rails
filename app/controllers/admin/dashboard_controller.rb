abc3ba43aef67d1e71e5147c9e32e915e899acd7
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-09-22 20:47:40 +0000

    implement admin dashboard counts for products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class Admin::DashboardController < ApplicationController
 
   def show
-    @products = Product.count
-    @categories = Category.count
+    @product_count = Product.count
+    @category_count = Category.count
   end
-  
+
 end
