9b0b38589d388918947ad0fa42a007e84bbd006d
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-09-23 17:32:58 +0000

    add sessions controller, routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   get '/signup', to: 'users#new'
   get '/users', to: 'users#create'
 
